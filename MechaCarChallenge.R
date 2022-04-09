b7ef09bef6e45150728a4c6dc7c56562ad55fd2d
mlblake105 <mlblake105@gmail.com>
2022-04-08 20:20:28 -0500

    Deliverable 1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,6 @@
+library(dplyr)
+
 MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table) #create linear model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table)) #summarize linear model
